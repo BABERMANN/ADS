ce5896a782d6e8d64a67aee35d66f2ac05297493
Cauã  Pereira <141373591+BABERMANN@users.noreply.github.com>
2025-08-07 17:02:13 -0300

    Update tipos de dados.c

--- apr2/tipos de dados.c
+++ apr2/tipos de dados.c
@@ -65,3 +65,101 @@ int main(){
     return 0;
 }
 
+ //4. Faça um programa que leia a idade de 4 pessoas e calcule a média
+
+int main(){
+     float num1,num2,num3,num4, media;
+     
+
+    printf("Digite a idade do primeiro :");
+    scanf("%f",&num1);
+
+    printf("Digite a idade do segundo :");
+    scanf("%f",&num2);
+
+    printf("Digite a idade do terceiro :");
+    scanf("%f",&num3);
+
+    printf("Digite a idade do quarto :");
+    scanf("%f", &num4);
+
+    media = (num1+num2+num3+num4) / 4 ;
+
+    printf("%2.0f + %2.0f + %2.0f + %2.0f = %2.2f",num1,num2,num3,num4,media);
+
+}
+
+//5. Faça um programa que leia o código (inteiro) e salário de 5 funcionários e
+//imprima o relatório com valores justificados à direita
+
+int main(){
+
+    int cod1,cod2,cod3;
+    float sal1,sal2,sal3;
+
+    printf("Digite o codigo do usuario (tres numeros): ");
+    scanf("%d", &cod1);
+
+    printf("Digite o salario do usuario :");
+    scanf("%f", &sal1);
+
+        printf("Digite o codigo do usuario (tres numeros): ");
+    scanf("%d", &cod2);
+
+    printf("Digite o salario do usuario :");
+    scanf("%f", &sal2);
+
+        printf("Digite o codigo do usuario (tres numeros): ");
+    scanf("%d", &cod3);
+
+    printf("Digite o salario do usuario :");
+    scanf("%f", &sal3);
+
+    printf("O salario do funcionario %03d e de %10.2f\nO salario do funcionario %03d e de %10.2f\nO salario do funcionario %03d e de %10.2f\n",cod1, sal1, cod2, sal2, cod3, sal3);
+}
+
+//6.Faça um programa para converter números de formato decimal para
+hexadecimal
+
+  int main (){
+        int num;
+
+     printf("Digite um numero para ser convertido para hexadecimal: ");
+     scanf("%d",&num);
+     
+     printf("Seu numero em hexadecimal e: %x", num);
+}
+
+
+//7.Faça um programa para calcular as raízes de uma equação do 1º grau (ax + b = 0).
+//Casos de teste:
+
+  int main(){
+
+        float A,B,raiz;
+
+        printf("Digite o valor de A: ");
+        scanf("%f", &A);
+
+        printf("Digite o valor de B:");
+        scanf("%f", &B);
+        
+        raiz = -B/A;
+
+        printf("Sua raiz de X e igual a %.2f", raiz);
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
