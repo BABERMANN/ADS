3cd513c1e64e21ce06af202d153f439e9eb37941
Cauã  Pereira <141373591+BABERMANN@users.noreply.github.com>
2025-08-18 22:02:41 -0300

    Create matriz.c

---
+++ apr2/matriz.c
@@ -0,0 +1,11 @@
+// 1- Escreva um programa que leia uma matriz 3x3 de númerosinteiros e imprima
+// a) Soma dos elementos
+// b) Maior e menor elemento
+// c) Diagonal principal
+// d) Diagonal secundária
+// e) Soma dos elementos de cada linha
+// f) Maior elemento de cada coluna
+// g) Matriz Transposta
+// h) Se é uma matriz simétrica
+// i) Valor do traço
+
