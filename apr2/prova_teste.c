95ceb80ddcb9761ed57818ca1f2f3f5df8695b4f
Cauã  Pereira <141373591+BABERMANN@users.noreply.github.com>
2025-09-18 23:41:23 -0300

    Update prova_teste.c

--- apr2/prova_teste.c
+++ apr2/prova_teste.c
@@ -1,44 +1,82 @@
-
-// 1.    Defina o tipo tpersonagem com campos para armazenar nome do personagem e nível de poder.
-
-// 2.    Escreva a função imprime_tabuleiro(tabuleiro), que imprime o tabuleiro com os personagens posicionados.
-
-// 3.    Escreva a função posiciona(linha, coluna, nome_personagem ,tabuleiro), que coloca o personagem no tabuleiro na posição indicada. A função deve retornar 1 se a operação for bem sucedida, ou zero se a posição for inválida.
-
-// 4.    Escreva a função ocorrencias(nome_personagem,tabuleiro), que retorna o número de vezes que o personagem aparece no tabuleiro.
-
-// 5.    Escreva a função poder_total(personagens,tabuleiro), que retorna a soma do nível de poder de todos os personagens localizados no tabuleiro.
-
-// 6.    Escreva a função ataca(linha,coluna,,personagens,tabuleiro), que lança uma ofensiva contra o personagem na posição indicada. A função apagar o personagem do tabuleiro e retornar o nível de poder do personagem atingido, ou zero se a posição estiver vazia ou a posição for inválida.
-
-
-
-
-
-
-
 #include <stdio.h>
+#include <string.h>
+
+typedef struct tpersonagem
+{
+    char nome[15];
+    int poder;
+} tpersonagem;
+
+void imprime_tabuleiro(char tabuleiro[10][10][15])
+{
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            if (strcmp(tabuleiro[i][j], "") != 0)
+                printf("[%s]", tabuleiro[i][j]);
+            else
+            {
+                printf("[-]");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int posiciona(int linha, int coluna, char nome_personagem[15], char tabuleiro[10][10][15])
+{
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            if (strcmp(tabuleiro[i][j], nome_personagem) == 0)
+            {
+                strcpy(tabuleiro[i][j], "");
+            }
+        }
+    }
+    if (strcmp(tabuleiro[linha][coluna], "") == 0)
+    {
+        strcpy(tabuleiro[linha][coluna], nome_personagem);
+        return 1;
+    } 
+    else
+    { 
+        return 0;
+    }
+}
 
 int main()
 {
-  char tabuleiro[10][10][15] = {
-         {"mago", "", "", "", "", "fada", "", "","","vampiro"},
-         {"", "", "dragao", "", "", "bruxa", "", "","",""},
-         {"", "", "", "", "mago", "", "", "","",""},
-         {"", "", "", "", "", "", "feiticeiro", "wendigo", "",""},
-         {"", "", "", "cavaleiro", "", "", "", "","",""},
-         {"", "", "", "", "duende", "", "", "","",""},
-         {"", "", "", "", "", "", "vampiro",  "","",""},
-         {"", "", "", "", "", "anjo", "lobisomem", "","",""},
-         {"", "", "gnomo", "", "", "", "", "","",""},
-         {"", "", "cavaleiro", "", "", "", "", "","","fada"}};
-           
+    int linha, coluna;
+    char nome_personagem[15], tabuleiro[10][10][15] = {
+                              {"mago", "", "", "", "", "fada", "", "", "", "vampiro"},
+                              {"", "", "dragao", "", "", "bruxa", "", "", "", ""},
+                              {"", "", "", "", "mago", "", "", "", "", ""},
+                              {"", "", "", "", "", "", "feiticeiro", "wendigo", "", ""},
+                              {"", "", "", "cavaleiro", "", "", "", "", "", ""},
+                              {"", "", "", "", "duende", "", "", "", "", ""},
+                              {"", "", "", "", "", "", "vampiro", "", "", ""},
+                              {"", "", "", "", "", "anjo", "lobisomem", "", "", ""},
+                              {"", "", "gnomo", "", "", "", "", "", "", ""},
+                              {"", "", "cavaleiro", "", "", "", "", "", "", "fada"}};
 
-  tpersonagem  personagens[30]= {{"anjo",17},{"bruxa",12},{"cavaleiro",20},{"dragao",18},{"duende",5},
-	{"fada",10},{"feiticeiro",12},{"gnomo",8},{"lobisomem",9},{"mago",15},{"princesa",4},
-	{"vampiro",13},{"wendigo",16}};
-	
-	
-	
-	return 0;
+    tpersonagem personagens[30] = {{"anjo", 17}, {"bruxa", 12}, {"cavaleiro", 20}, {"dragao", 18}, {"duende", 5}, {"fada", 10}, {"feiticeiro", 12}, {"gnomo", 8}, {"lobisomem", 9}, {"mago", 15}, {"princesa", 4}, {"vampiro", 13}, {"wendigo", 16}};
+
+    imprime_tabuleiro(tabuleiro);
+
+
+    printf("Digite o nome do personagem");
+    scanf("%s", &nome_personagem);
+    printf("Digite a linha");
+    scanf("%d", &linha);
+    printf("Digite a coluna");
+    scanf("%d", &coluna);
+
+    posiciona(linha, coluna, nome_personagem, tabuleiro);
+
+     imprime_tabuleiro(tabuleiro);
+
+    return 0;
 }
