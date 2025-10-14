7f71cb8b722d6abd89077a3bd5ed5db949dd608e
Cauã  Pereira <141373591+BABERMANN@users.noreply.github.com>
2025-10-13 22:24:05 -0300

    Update Alocacao_Dinamica.c

--- apr2/Alocacao_Dinamica.c
+++ apr2/Alocacao_Dinamica.c
@@ -37,3 +37,56 @@ int main() {
     return 0;
 }
 ------------------------------------------------------------------------------------
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int codigo;
+    float nota1, nota2;
+    int faltas;
+} aluno;
+
+void le_aluno(aluno *a) {
+    printf("codigo: ");
+    scanf("%d", &(a->codigo));
+    printf("nota 1: ");
+    scanf("%f", &(a->nota1));
+    printf("nota 2: ");
+    scanf("%f", &(a->nota2));
+    printf("faltas: ");
+    scanf("%d", &(a->faltas));
+}
+
+aluno* le_turma(int N) {
+    aluno *turma = (aluno*) malloc(N * sizeof(aluno));
+    for (int i = 0; i < N; i++) {
+        printf("\nAluno %d\n", i + 1);
+        le_aluno(&turma[i]);
+    }
+    return turma;
+}
+
+void imprime_aluno(aluno *a) {
+    printf("%d\t%.1f\t%.1f\t%d\n", a->codigo, a->nota1, a->nota2, a->faltas);
+}
+
+void imprime_turma(aluno *turma, int qtd_alunos) {
+    printf("\nCodigo\tNota 1\tNota 2\tFaltas\n");
+    for (int i = 0; i < qtd_alunos; i++) {
+        imprime_aluno(&turma[i]);
+    }
+}
+
+int main() {
+    int N;
+    printf("Digite o tamanho da turma: ");
+    scanf("%d", &N);
+
+    aluno *turma = le_turma(N);
+    imprime_turma(turma, N);
+
+    free(turma);
+    return 0;
+}
+    
