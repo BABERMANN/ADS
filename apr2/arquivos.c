090ea9bd016fa8a30c8056642243b004abe5d1d8
Cauã  Pereira <141373591+BABERMANN@users.noreply.github.com>
2025-11-07 17:02:06 -0300

    Update arquivos.c

--- apr2/arquivos.c
+++ apr2/arquivos.c
@@ -1,51 +1,96 @@
+
+
 #include <stdio.h>
-#include <stdlib.h>
-typedef int tchave;
+#include <stdlib.h> 
+#define tchave int
 
-typedef struct 
-{
+typedef struct {
     tchave chave;
-    float valor;
-    char categoria;
-}registros;
+    float valor;   
+    char categoria; 
+} registro;
 
-// conferir o tamanho do arquivo
-int main(){
-FILE* arq;
-int qntd_registros;
-long tamanho_arq;
+typedef struct {
+    int tamanho;
+    registro *item;
+} tabela;
 
-arq = fopen("produtos.jlf","rb");
-if(arq == NULL){
-    printf("Erro ao abrir o arquivo:");
-    return 1;
+
+
+void imprimir_registro(registro a) {
+    
+    printf("Codigo: %4d", a.chave);
+    printf("\tValor: %5.2f", a.valor);
+    printf("\tCategoria: %c\n", a.categoria);
 }
 
-fseek(arq, 0, SEEK_END);
-tamanho_arq = ftell(arq);
-qntd_registros = tamanho_arq / sizeof(registros);
-
-printf("Tamanho do arquivo: %ld Bytes\n",tamanho_arq);
-printf("Quantidade de registros: %d",qntd_registros);
-//#########################################################
-
-//printar as informacoes no arquivo
-
-registros registro_temp;
-int i = 1; //contador para ver qual registro estamos lendo
-
-rewind(arq);
-
-while(fread(&registro_temp, sizeof(registros), 1, arq) == 1){ 
-    printf("registro %d\n", i);
-    printf("chave : %d\n", registro_temp.chave);
-    printf("valor : %f\n", registro_temp.valor);
-    printf("categoria: %c\n", registro_temp.categoria);
-    printf("\n");
-
-    i++;
+void imprimir_tabela(tabela T) {
+    int i;
+    printf("=========================================\n");
+    for (i = 0; i < T.tamanho; i++) {
+        imprimir_registro(T.item[i]);
+    }
+    printf("=========================================\n");
 }
 
-fclose(arq);
 
+tabela ler_tabela() {
+    FILE* arq;
+    tabela T;
+    T.tamanho = 0; 
+    T.item = NULL; 
+    long tamanho_arq;
+
+    // Lendo o seu arquivo
+    arq = fopen("produtos.jlf", "rb"); 
+    
+    if(arq == NULL) {
+        fprintf(stderr, "Erro ao abrir o arquivo 'produtos.jlf'\n");
+        return T; 
+    }
+
+    fseek(arq, 0, SEEK_END);
+    tamanho_arq = ftell(arq);
+
+    T.tamanho = tamanho_arq / sizeof(registro);
+
+    
+    T.item = (registro*) malloc(T.tamanho * sizeof(registro));
+    if (T.item == NULL) {
+        fprintf(stderr, "Erro ao alocar memoria!\n");
+        fclose(arq);
+        T.tamanho = 0;
+        return T; 
+    }
+
+    rewind(arq);
+    fread(T.item, sizeof(registro), T.tamanho, arq);
+
+    fclose(arq);
+    return T;
+}
+
+
+void selection_sort(tabela t) {
+    int i, j, min, n;
+    registro aux, *v; 
+
+    v = t.item;
+    n = t.tamanho;
+
+    for (i = 0; i < n - 1; i++) {
+        min = i; 
+        
+        for (j = i + 1; j < n; j++) {
+            if (v[j].chave < v[min].chave) {
+                min = j;
+            }
+        }
+
+        if (i != min) {
+            aux = v[i];
+            v[i] = v[min];
+            v[min] = aux;
+        }
+    }
 }
