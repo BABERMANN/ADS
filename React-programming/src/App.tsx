efd7bb50c543568e177674e1b2fa83af38ebb3b6
BABERMANN <cauahabermann7@gmail.com>
2026-01-28 23:32:45 -0300

    feat:completo com create, read e delete

--- React-programming/src/App.tsx
+++ React-programming/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
+import type { FormEvent } from 'react'
 import { Card } from './components/Card'
-
 
 interface Character {
   id: number;
@@ -11,39 +11,96 @@ interface Character {
 }
 
 function App() {
-  
   const [characters, setCharacters] = useState<Character[]>([])
 
+ 
+  // Guardam o que você está digitando nos inputs
+  const [novoNome, setNovoNome] = useState('')
+  const [novaEspecie, setNovaEspecie] = useState('')
+
+  // Carregar dados da API
   useEffect(() => {
     async function carregarDados() {
       const resposta = await fetch('https://rickandmortyapi.com/api/character')
       const dados = await resposta.json()
-      
-     
       setCharacters(dados.results)
     }
-
     carregarDados()
   }, [])
+
+  // FunçAo de Deletar 
+  function deletarPersonagem(id: number) {
+    const novaLista = characters.filter(item => item.id !== id)
+    setCharacters(novaLista)
+  }
+
+  // (CREATE)
+  function adicionarPersonagem(event: FormEvent) {
+    event.preventDefault() // Evita que a página recarregue
+
+    //Montamos o novo boneco
+    const novoPersonagem: Character = {
+      id: Math.random(), // Gera um ID aleatOrio 
+      name: novoNome,
+      species: novaEspecie,
+      status: "Alive", // PadrAo
+      image: "https://rickandmortyapi.com/api/character/avatar/19.jpeg" 
+    }
+
+    
+    
+    setCharacters([novoPersonagem, ...characters])
+
+    // Limpa os inputs
+    setNovoNome('')
+    setNovaEspecie('')
+  }
 
   return (
     <div style={{ padding: '20px' }}>
       <h1>Galeria Rick and Morty</h1>
-      
-      
+
+      {/* --- formulario de cadastro --- */}
+      <form onSubmit={adicionarPersonagem} style={{ marginBottom: '30px', background: '#f0f0f0', padding: '20px', borderRadius: '8px' }}>
+        <h3>Adicionar Novo Personagem</h3>
+        
+        <div style={{ display: 'flex', gap: '10px' }}>
+          <input 
+            type="text" 
+            placeholder="Nome do Personagem" 
+            value={novoNome}
+            onChange={e => setNovoNome(e.target.value)}
+            style={{ padding: '8px' }}
+            required
+          />
+          
+          <input 
+            type="text" 
+            placeholder="Espécie (ex: Human, Alien)" 
+            value={novaEspecie}
+            onChange={e => setNovaEspecie(e.target.value)}
+            style={{ padding: '8px' }}
+            required
+          />
+
+          <button type="submit" style={{ padding: '8px 20px', background: 'green', color: 'white', border: 'none', cursor: 'pointer' }}>
+            Criar
+          </button>
+        </div>
+      </form>
+
+      {/* lista de cards */}
       <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px' }}>
-        
-        
         {characters.map(personagem => (
           <Card 
-            key={personagem.id} 
+            key={personagem.id}
             name={personagem.name}
             status={personagem.status}
             species={personagem.species}
             image={personagem.image}
+            onDelete={() => deletarPersonagem(personagem.id)}
           />
         ))}
-
       </div>
     </div>
   )
--- React-programming/src/components/Card.tsx
+++ React-programming/src/components/Card.tsx
@@ -1,16 +1,47 @@
-interface CardProps { 
-    name:string;
-    status:string;
-    species:string;
-    image:string;
+interface CardProps {
+  name: string;
+  status: string;
+  species: string;
+  image: string;
+  // Nova Prop: Uma função que não retorna nada (void)
+  // e o botao de pânico
+  onDelete: () => void;
 }
 
-export function Card({ name, status, species, image }: CardProps) {
+export function Card({ name, status, species, image, onDelete }: CardProps) {
   return (
-    <div style={{ border: '1px solid #333', padding: '10px', borderRadius: '8px', width: '200px' }}>
+    <div style={{ 
+      border: '1px solid #333', 
+      padding: '10px', 
+      borderRadius: '8px', 
+      width: '200px', 
+      display: 'flex', 
+      flexDirection: 'column', 
+      gap: '10px' 
+    }}>
       <img src={image} alt={name} style={{ width: '100%', borderRadius: '4px' }} />
-      <h3>{name}</h3>
-      <p>{status} - {species}</p>
+      
+      <div>
+        <h3 style={{ margin: 0 }}>{name}</h3>
+        <p style={{ margin: 0, color: '#666' }}>{status} - {species}</p>
+      </div>
+
+      {/* Botao de Deletar */}
+      
+      <button 
+        onClick={onDelete} 
+        style={{ 
+          backgroundColor: 'tomato', 
+          color: 'white', 
+          border: 'none', 
+          padding: '8px', 
+          borderRadius: '4px', 
+          cursor: 'pointer',
+          fontWeight: 'bold'
+        }}
+      >
+        🗑️ Remover
+      </button>
     </div>
   )
 }
