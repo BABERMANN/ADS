a9ca00240f1f2e798c9ec568a254337afaa39841
Caua Matheus Habermann Pereira <cauahabermann7@gmail.com>
2026-08-19 17:46:14 -0300

    Add initial setup for Java-Learning project

---
+++ Java-Learning/src/verify_email.java
@@ -0,0 +1,28 @@
+public class verify_email {
+
+    public static boolean valid(String email) {
+        if(email == null || email.trim().isEmpty()) return false;
+
+        int first_occurrence = email.indexOf("@");
+        int second_occurrence = email.lastIndexOf("@");
+
+        if(first_occurrence <= 0 || first_occurrence != second_occurrence || email.length() - 1 == first_occurrence) return false;
+
+        String local = email.substring(0, first_occurrence);
+        String domain = email.substring(first_occurrence + 1);
+
+        if (!domain.contains(".") || domain.startsWith(".") || domain.endsWith(".")) return false;
+
+        if (domain.indexOf(".") == 0) return false;
+
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        String emailTeste = ".@.....";
+        boolean resultado = valid(emailTeste);
+
+        System.out.println("O e-mail '" + emailTeste + "' é válido? " + resultado);
+    }
+}
